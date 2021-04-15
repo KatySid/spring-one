06a650f35b21276d74516cd42f0da5e44b705488
KatySid <whitekaty666@gmail.com>
2021-04-15 23:30:27 +0400

    Lesson 3

---
+++ src/main/java/ru/geekbrains/spring/one/controllers/ProductController.java
@@ -0,0 +1,49 @@
+package ru.geekbrains.spring.one.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.spring.one.model.Product;
+import ru.geekbrains.spring.one.services.ProductService;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/all")
+    public String showAllProductsPage(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products";
+    }
+
+    @GetMapping("/{id}")
+    public String showProductInfo(@PathVariable(name = "id") Long id, Model model) {
+        Optional<Product> product = productService.findOneById(id);
+        if (product.isPresent()) {
+            model.addAttribute("product", product.get());
+        }
+        return "product_info";
+    }
+
+    @GetMapping("/create")
+    public String showCreator() {
+        return "create_product_form";
+    }
+
+    @PostMapping("/create")
+    public String createNewProduct(@RequestParam Long id, @RequestParam String title, @RequestParam int cost) {
+        Product product = new Product(id, title, cost);
+        productService.save(product);
+        return "redirect:/products/all";
+    }
+}
